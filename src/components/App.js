455168f8783f582448ab0de9edebb817a018dfaf
Kshitinjay <kshitinjay20@gmail.com>
2021-02-16 13:30:20 +0530

    checking

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,29 @@
 import React from "react";
 import "../styles/App.css";
 
-const App = () => {
-  const cityList = [
-    { name: "Goa", country: "India" },
-    { name: "Amsterdam", country: "Netherlands" },
-    { name: "New York", country: "USA" },
-    { name: "Darjeeling", country: "India" },
-    { name: "Tokyo", country: "Japan" },
-    { name: "Lonavala", country: "India" },
-    { name: "Brandenburg Gate", country: "Germany" },
-    { name: "Reichstag Building", country: "Germany" },
-    { name: "Museum Island", country: "Germany" },
-    { name: "Munnar", country: "India" },
-    { name: "Leh Ladakh", country: "India" },
-    { name: "Goa", country: "India" },
-    { name: "Agra", country: "India" },
-    { name: "Dalhousie", country: "India" },
-    { name: "Coorg", country: "India" },
-    { name: "Rome", country: "Italy" },
-    { name: "Milan", country: "Italy" },
-    { name: "Venice", country: "Italy" },
-    { name: "Varanasai", country: "India" },
-    { name: "Jaipur", country: "India" },
-    { name: "The Hofburg", country: "Austria" },
-    { name: "Belvedere Palace", country: "Austria" },
-    { name: "St. Stephen Cathedral", country: "Austria" },
-    { name: "Kahna National Park", country: "India" },
-    { name: "Amritsar", country: "India" },
-    { name: "Mussoorie", country: "India" },
-    { name: "Mount Abu", country: "India" },
-    { name: "Tirupati", country: "India" },
-  ];
-  const onlyIndia = cityList.filter((element, index, arr) => {
-    return element.country === "India";
-  });
-  let testArray = [];
-  const resultCity = onlyIndia.map((element, index, arr) => {
-    if (!testArray.includes(element.name)) {
-      testArray.push(element.name);
-      return <li key={`location${index + 1}`}>{element.name}</li>;
-    }
-  });
+const citylist = [
+  { name: "Goa", country: "India" },
+  { name: "Amsterdam", country: "Netherlands" },
+  { name: "New York", country: "USA" },
+  { name: "Darjeeling", country: "India" },
+  { name: "Tokyo", country: "Japan" },
+  { name: "Lonavala", country: "India" },
+];
 
+function App() {
+  //const[state,setState]=useState(1);
   return (
-    <div id="main">
-      <ol>{resultCity}</ol>
+    <div>
+      {citylist
+        .filter((person) => person.country === "India")
+        .map((filteredPerson) => (
+          <ol style={{ listStyleType: "number" }}>
+            <li>{filteredPerson.name}</li>
+          </ol>
+        ))}
+      ;
     </div>
   );
-};
+}
+
 export default App;
